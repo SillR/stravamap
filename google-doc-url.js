28f735fa515aab274ce730bf038820ebbbd0b350
SillR <144783479+SillR@users.noreply.github.com>
2023-10-13 14:15:47 +0200

    Create google-doc-url.js

---
+++ google-doc-url.js
@@ -0,0 +1,4 @@
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-9nwU4ky6kxOGwfY8frdp56NkILao0auUi9hjbGg-BI/edit#gid=0';
+
+// insert your own Google Sheets API key from https://console.developers.google.com
+var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
