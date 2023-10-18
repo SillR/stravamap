625a618dbff3c8fbd98d3233cd42d3bcf84f7c55
SillR <144783479+SillR@users.noreply.github.com>
2023-10-18 17:03:13 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,4 +1,4 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1-9nwU4ky6kxOGwfY8frdp56NkILao0auUi9hjbGg-BI/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1Iv4_cxV2Qn2Wtz0HYmIj6CX1McVpSVQMa9uI65a4LP0/edit#gid=164271551';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
